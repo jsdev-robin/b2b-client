c537fe68af76941329eb70890baa5876069bd9a4
jsdev-robin <jsdev.robin@gmail.com>
2026-03-16 21:15:20 +0600

    feat: completed

--- apps/b2b/src/components/site/protected/layouts/app-sidebar.tsx
+++ apps/b2b/src/components/site/protected/layouts/app-sidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { AudioWaveform, Command, GalleryVerticalEnd, Grid } from 'lucide-react';
+import { GalleryVerticalEnd, Grid } from 'lucide-react';
 import * as React from 'react';
 
 import {
@@ -16,26 +16,11 @@ import { TeamSwitcher } from './team-switcher';
 
 // This is sample data.
 const data = {
-  user: {
-    name: 'shadcn',
-    email: 'm@example.com',
-    avatar: '/avatars/shadcn.jpg',
-  },
   teams: [
     {
-      name: 'Acme Inc',
+      name: 'Devmun',
       logo: GalleryVerticalEnd,
       plan: 'Enterprise',
-    },
-    {
-      name: 'Acme Corp.',
-      logo: AudioWaveform,
-      plan: 'Startup',
-    },
-    {
-      name: 'Evil Corp.',
-      logo: Command,
-      plan: 'Free',
     },
   ],
   navMain: [
--- apps/b2b/src/components/site/protected/pages/service/index.tsx
+++ apps/b2b/src/components/site/protected/pages/service/index.tsx
@@ -7,11 +7,11 @@ const Services = () => {
   return (
     <section>
       <div className="wrapper">
-        <div className="grid grid-cols-3 gap-4">
-          <div className="col-span-1">
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+          <div className="lg:col-span-1">
             <ServiceCreate />
           </div>
-          <div className="col-span-2">
+          <div className="lg:col-span-2">
             <ServiceList />
           </div>
         </div>
