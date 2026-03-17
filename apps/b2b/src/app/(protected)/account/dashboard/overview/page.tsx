7496c816ccb83fc12bfc804dd5173bf2958e3c95
jsdev-robin <jsdev.robin@gmail.com>
2026-03-17 16:35:08 +0600

    feat: add server active logic for vercel by polling

--- apps/b2b/src/app/(protected)/account/dashboard/overview/page.tsx
+++ apps/b2b/src/app/(protected)/account/dashboard/overview/page.tsx
@@ -1,8 +1,21 @@
 'use client';
 
 import Services from '@/components/site/protected/pages/service';
+import { gatewayApi } from '@/lib/features/api/gatewayApi';
 
 const Home = () => {
+  const { data: authData } = gatewayApi.usePollingAuthQuery(undefined, {
+    pollingInterval: 60000,
+  });
+
+  const { data: b2bData } = gatewayApi.usePollingB2BQuery(undefined, {
+    pollingInterval: 90000,
+  });
+
+  // This is for server active,
+  console.log(authData);
+  console.log(b2bData);
+
   return (
     <>
       <Services />
